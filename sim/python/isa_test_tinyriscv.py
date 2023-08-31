62acc404f7b66bf2d0d083c7faed3e2095c38022
DuBirdFly <2937823748@qq.com>
2023-09-01 04:52:23 +0800

    update

--- sim/python/isa_test_tinyriscv.py
+++ sim/python/isa_test_tinyriscv.py
@@ -4,7 +4,7 @@ from packages.MyFuc import run_cmd
 # 常数 ########################################################################
 # True: 使用新的测试文件(riscv-compliance)
 # False: 使用旧的测试文件(riscv-isa)
-IS_NEW_TEST = False
+IS_NEW_TEST = True
 
 PATH_CWD = os.getcwd().replace('\\', '/')
 PATH_SIM = f"{PATH_CWD}/sim"
@@ -20,7 +20,7 @@ MEM_FILE = f"{PATH_SIM}/output/inst.data"
 # MEM_FILE = f"C:/Users/29378/Desktop/aa/inst.data"
 
 # 指令: Bin2Mem.py ############################################################
-Bin2MemArvg = ["python", f"{PATH_SIM}/py/packages/Bin2Mem.py"]
+Bin2MemArvg = ["python", f"{PATH_SIM}/python/packages/Bin2Mem.py"]
 
 if IS_NEW_TEST:
     Bin2MemArvg.append(f"{PATH_BIN_FILE}")
@@ -33,7 +33,7 @@ cmd = ' '.join(Bin2MemArvg)
 run_cmd(cmd)
 
 # 指令: Sim.py ################################################################
-SimArvg = ["python", f"{PATH_SIM}/py/packages/Sim.py"]
+SimArvg = ["python", f"{PATH_SIM}/python/packages/Sim.py"]
 SimArvg.append(f"{PATH_SIM}/output/vvp_script.vvp")
 SimArvg.append(f"{PATH_CWD}/tinyriscv/rtl/core")
 if IS_NEW_TEST:
@@ -53,7 +53,7 @@ if IS_NEW_TEST:
     path_ref_dir = os.path.dirname(PATH_BIN_FILE).replace('build_generated', 'riscv-test-suite')
     name_ref_file = os.path.basename(PATH_BIN_FILE).replace(".elf.bin", ".reference_output")
 
-    cmd = ["python", f"{PATH_SIM}/py/packages/FileSimilar.py"]
+    cmd = ["python", f"{PATH_SIM}/python/packages/FileSimilar.py"]
     cmd.append(f"{path_ref_dir}/references/{name_ref_file}")    # 官方的参考文件路径
     cmd.append(f"{PATH_SIM}/output/signature.txt")              # 我用TestBench捕获的数据
 
